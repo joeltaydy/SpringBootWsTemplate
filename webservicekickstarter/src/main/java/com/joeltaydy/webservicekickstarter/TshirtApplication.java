4deb9a4e3b682d591764243fc97440ca4fb55e1f
joel <joel.tay.2016@sis.smu.edu.sg>
2020-04-22 17:50:31 +0800

    Initializer
    Spring Initializer with Spring boot web starter

---
+++ webservicekickstarter/src/main/java/com/joeltaydy/webservicekickstarter/TshirtApplication.java
@@ -0,0 +1,13 @@
+package com.joeltaydy.webservicekickstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TshirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TshirtApplication.class, args);
+	}
+
+}
